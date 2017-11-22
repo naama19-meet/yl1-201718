813fb2cd047c39c0e184a88fb612134ce37b8ae5
naama19-meet <naama19@meet.mit.edu>
2017-11-22 19:20:09 +0200

    shit

---
+++ lab5/lab5.py
@@ -0,0 +1,26 @@
+from turtle import *
+colormode(255)
+ 
+ class Square(turtle):
+ 	"""docstring for Square"""
+ 	def __init__(self, zise):
+ 		Turtle.__init__(self)
+ 		self.zise = zise
+ 		
+
+
+
+
+
+
+
+
+
+#class Hexagon(turtle):
+#	"""docstring for Hexagon"""
+#	def __init__(self, size):
+#		
+#		self.size = size
+
+		
+ 		
