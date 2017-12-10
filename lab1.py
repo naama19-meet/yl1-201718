7e5ca41d4faefb950fbc1400921ba8bf10e9b8e3
naama19-meet <naama19@meet.mit.edu>
2017-12-10 18:47:38 +0200

    lab6

---
+++ lab1.py
@@ -0,0 +1,22 @@
+
+#h = " naama "*100
+#print(h)
+
+#number1 = 6
+#print(number1
+
+#number2 = number1/2
+#print(number2) 
+
+#sum=0
+#list1 = [2,4,6]
+#for f in list1:
+	#sum=sum+f
+	#print(list1[0]+list1[1]+list1[2])
+
+#print(sum)
+
+
+import turtle
+
+turtl.mainloop()
--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,5 +1,6 @@
 from turtle import *
 import random
+import math 
 
 class Ball(Turtle):
 	"""docstring for ball"""
@@ -12,11 +13,46 @@ class Ball(Turtle):
 		self.speed(speed)
 
 
-ball1 = Ball(10, "blue", 10)
-ball2 = Ball(11, "pink", 5)
+ball1 = Ball(10, "green", 1)
+ball2 = Ball(5, "red", 6)
 
-def chack_collision(ball1,ball2):
-	
+#def check_collision(ball1,ball2):
+#	d = math.sqrt(math.pow((ball1.xcor()-ball2.xcor()),2) + math.pow((ball1.ycor()-ball2.ycor()),2))
+#	if d <= ball1.rdius+ ball2.rdius:
+#		print("collision!!!")
+#	else:
+#		print("not collision  :(")
+#
+#check_collision(ball1,ball2)
+#mainloop()
+ 
 
-	math.sqrt(math.pow((ball1.xcor()-ball2.xcor()),2) + math.pow((ball1.ycor()-ball2.ycor()),2))
+
+
+
+
+
+
+
+
+balls = []
+balls.append(ball1)
+balls.append(ball2)
+
+#print(balls)
+
+
+def check_collision(balls):
+	d = math.sqrt(math.pow((balls[0].xcor()-balls[1].xcor()),2) + math.pow((balls[0].ycor()-balls[1].ycor()),2))
+	if d <= balls[0].rdius+ balls[1].rdius:
+		if balls[0].rdius < balls[1].rdius:
+			balls[0].goto(50,50)
+		else:
+			balls[1].goto(50,50)
+
+
+check_collision(balls)
+
+
+
 mainloop()
